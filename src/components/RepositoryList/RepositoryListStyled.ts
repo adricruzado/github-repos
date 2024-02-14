cd01c9f23392f6176a33c4ff0b0020fe3a74c2a4
Adri Cruzado <adricruzado96@gmail.com>
2024-02-14 14:59:29 +0100

    Add RepositoryList component styles

---
+++ src/components/RepositoryList/RepositoryListStyled.ts
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+const RepositoryListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 25px;
+`;
+
+export default RepositoryListStyled;
